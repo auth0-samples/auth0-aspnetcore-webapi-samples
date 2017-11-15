858451ecddb5d3f4930f04d4044381d912014d06
Jerrie Pelser <jerrie@jerriepelser.com>
2017-11-15 14:14:02 +0700

    Split authorization requirement and handler

--- Quickstart/01-Authorization/HasScopeRequirement.cs
+++ Quickstart/01-Authorization/HasScopeRequirement.cs
@@ -1,34 +1,17 @@
-using System.Linq;
-using System.Threading.Tasks;
+using System;
 using Microsoft.AspNetCore.Authorization;
 
 namespace WebAPIApplication
 {
-    public class HasScopeRequirement : AuthorizationHandler<HasScopeRequirement>, IAuthorizationRequirement
+    public class HasScopeRequirement : IAuthorizationRequirement
     {
-        private readonly string issuer;
-        private readonly string scope;
+        public string Issuer { get; }
+        public string Scope { get; }
 
         public HasScopeRequirement(string scope, string issuer)
         {
-            this.scope = scope;
-            this.issuer = issuer;
-        }
-
-        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
-        {
-            // If user does not have the scope claim, get out of here
-            if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == issuer))
-                return Task.CompletedTask;
-
-            // Split the scopes string into an array
-            var scopes = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == issuer).Value.Split(' ');
-
-            // Succeed if the scope array contains the required scope
-            if (scopes.Any(s => s == scope))
-                context.Succeed(requirement);
-
-            return Task.CompletedTask;
+            Scope = scope ?? throw new ArgumentNullException(nameof(scope));
+            Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
         }
     }
 }
--- Quickstart/01-Authorization/Startup.cs
+++ Quickstart/01-Authorization/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebAPIApplication
 {
@@ -43,6 +44,9 @@ namespace WebAPIApplication
                 options.AddPolicy("read:messages", policy => policy.Requirements.Add(new HasScopeRequirement("read:messages", domain)));
                 options.AddPolicy("create:messages", policy => policy.Requirements.Add(new HasScopeRequirement("create:messages", domain)));
             });
+
+            // register the scope authorization handler
+            services.AddSingleton<IAuthorizationHandler, HasScopeHandler>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
